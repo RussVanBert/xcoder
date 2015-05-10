69fe3748947b221d357c4278273ce09ebf68f63f
Max Chuquimia <max.chuquimia@tigerspike.com>
2015-05-11 09:33:40 +1000

    Debugging is easier when you have the start and end of a string delimited

--- lib/xcode/configuration_owner.rb
+++ lib/xcode/configuration_owner.rb
@@ -25,7 +25,7 @@ module Xcode
     #
     def config(name)
       config = configs.select {|config| config.name == name.to_s }.first
-      raise "No such config #{name}, available configs are #{configs.map {|c| c.name}.join(', ')}" if config.nil?
+      raise "No such config '#{name}', available configs are #{configs.map {|c| c.name}.join(', ')}" if config.nil?
       yield config if block_given?
       config
     end
